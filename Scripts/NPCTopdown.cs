2f94d998af14d2f137e990577b1d1d570f5a3b70
tyruji <tyruji@gmail.com>
2024-07-08 13:28:17 +0200

    Added npc topdown movement and patrolling along points

--- Scripts/NPCTopdown.cs
+++ Scripts/NPCTopdown.cs
@@ -1,8 +1,68 @@
 using Godot;
 using System;
+using static CharacterStates;
 
-public partial class NPCTopdown : CharacterBody2D
+public partial class NPCTopdown : CharacterBody2D, IMoveable, IStateHolder//, IAnimatable
 {
     // Integrate like a NavMesh agent and shit here
     // make functions to move the npc to a chosen position
+
+    [Export]
+    public float Speed { get; set; } = 200f;
+
+    [Export]
+    public Path2D Path { get; set; } = null;
+
+    [Export]
+    public float MinPointDistance { get; set; } = 50f;
+
+    public Vector2 TargetPosition { set => _navAgent.TargetPosition = value;}
+
+    public Vector2 Direction { get; set; }
+
+    public Vector2 PreviousDirection { get; set; }
+
+    public State State { get; set; } = IDLE_STATE;
+
+    private NavigationAgent2D _navAgent = null;
+
+    private int _currentPointIdx = 0;
+
+    public override void _Ready()
+    {
+        _navAgent = GetNode<NavigationAgent2D>( "NavigationAgent2D" );
+
+    }
+
+    public override void _Process( double delta )
+    {
+        HandlePathFollow();
+
+        Direction = ToLocal( _navAgent.GetNextPathPosition() ).Normalized();
+
+        Velocity = Speed * Direction;
+
+        State.Handle( this );
+    }
+
+    public override void _PhysicsProcess(double delta)
+    {
+        MoveAndSlide();
+    }
+
+    private void HandlePathFollow()
+    {
+            // If there is no path to follow, dont do it
+        if( Path == null || Path.Curve.PointCount == 0 ) return;
+
+        Vector2 target = Path.Curve.GetPointPosition( _currentPointIdx );
+    
+        if( target.DistanceSquaredTo( GlobalPosition ) <= MinPointDistance * MinPointDistance )
+        {
+            _currentPointIdx = ( _currentPointIdx + 1) % Path.Curve.PointCount;
+            target = Path.Curve.GetPointPosition( _currentPointIdx );
+        }
+
+        TargetPosition = target;
+    }
 }
