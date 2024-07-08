9bc8fb00d489b86047b6f5d6f48d771cf9c4d9a6
tyruji <tyruji@gmail.com>
2024-07-08 11:45:17 +0200

    Added topdown class

---
+++ Scripts/NPCTopdown.cs
@@ -0,0 +1,8 @@
+using Godot;
+using System;
+
+public partial class NPCTopdown : CharacterBody2D
+{
+    // Integrate like a NavMesh agent and shit here
+    // make functions to move the npc to a chosen position
+}
