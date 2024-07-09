d2eb3fc9124ae9e481f8599b948a443edc2a925f
abdulbondibull <91325191+abdulbondibull@users.noreply.github.com>
2024-07-09 20:38:28 +0200

    Fix===

--- Scripts/PlayerTopdown.cs
+++ Scripts/PlayerTopdown.cs
@@ -6,11 +6,11 @@ public partial class PlayerTopdown : PlayerBase//, IAnimatable
 {
 /*
 
-    [Export]
-    public AnimationPlayer AnimationPlayer { get; private set; } = null;
+	[Export]
+	public AnimationPlayer AnimationPlayer { get; private set; } = null;
 
-    [Export]
-    public Sprite2D Sprite { get; private set; } = null;
-    */
+	[Export]
+	public Sprite2D Sprite { get; private set; } = null;
+	*/
 
 }
