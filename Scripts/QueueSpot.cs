bcd13ecf4284a55d5edd3dbcfc914d4f78eca4f4
tyruji <tyruji@gmail.com>
2024-07-10 16:22:18 +0200

    Coffee queue spot added

---
+++ Scripts/QueueSpot.cs
@@ -0,0 +1,18 @@
+using Godot;
+using System;
+
+public partial class QueueSpot : Sprite2D
+{
+	
+	private void OnBodyEntered( Node2D body )
+	{
+			// Replace with function body.
+		
+	}
+
+}
+
+
+
+
+
